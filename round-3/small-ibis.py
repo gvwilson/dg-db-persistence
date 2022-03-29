5cd6a60a46761b2583f5571096a9b192698a19f2
Greg Wilson <gvwilson@third-bit.com>
2022-03-29 08:46:02 -0400

    Updating based on recent adventures

--- round-3/small-ibis.py
+++ round-3/small-ibis.py
@@ -1,39 +1,26 @@
 import ibis
 import sys
 
-def show(title, records):
-    print(title)
-    print(records)
-
 conn = ibis.sqlite.connect(sys.argv[1])
 person = conn.table("Person")
 visited = conn.table("Visited")
 survey = conn.table("Survey")
 
-q = visited\
-    .group_by("site")\
-    .aggregate(visited.site.count().name("number"))
-show("Number of visits per site", q.execute())
-
-q = visited\
-    .inner_join(survey, visited["id"]==survey["taken"])\
-    .group_by([visited.site, survey.quant])\
-    .aggregate(visited.site.count().name("number"))
-show("Number of readings of each type for each site", q.execute())
-
+print("This works, but has NULLs in the 'dated' column")
 q = person\
     .inner_join(survey, person.id==survey.person)\
     .inner_join(visited, visited.id==survey.taken)\
     .group_by([person.id, visited.dated, survey.quant])\
     .aggregate(survey.reading.max().name("largest"))
-show("Highest reading of each type by each person on each day (simple)", q.execute())
 print(ibis.sqlite.compile(q))
+print(q.execute())
 
-visited = visited.filter(visited.dated.notnull())
+print("This attempt to filter the NULLs doesn't work")
 q = person\
     .inner_join(survey, person.id==survey.person)\
     .inner_join(visited, visited.id==survey.taken)\
+    .filter(person.id.notnull())\
     .group_by([person.id, visited.dated, survey.quant])\
     .aggregate(survey.reading.max().name("largest"))
-show("Highest reading of each type by each person on each day", q.execute())
 print(ibis.sqlite.compile(q))
+print(q.execute())
